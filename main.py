3e03383dbc37b2286094b6891793e7d1785008c3
jansek2 <159570587+jansek2@users.noreply.github.com>
2024-09-30 18:08:26 +0100

    Update main.py
    meme_dict zadanie, dodalem 3 angielskie.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("HELLO")
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            "BOUNCE": "opuścić miejsce (angielski)",
+            "SCRAN": "jedzenie (angielski)",
+            "PEAS": "pieniądze (angielski)",
+            }
+
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Zlowo nieznane")
